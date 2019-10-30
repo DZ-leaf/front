a51fa95edd5916e599bb1c0383599e10fbf811e1
az0325 <jy960325@gmail.com>
2019-10-30 17:02:27 +0900

    no message

--- screens/calendar/CalendarScreen.js
+++ screens/calendar/CalendarScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Calendars from '../components/Calendars'
+import Calendars from '../../components/Calendars';
 
 class CalendarScreen extends Component {
     
