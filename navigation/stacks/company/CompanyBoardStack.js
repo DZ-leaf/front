02ab62f56aecd157dfe334e78f8ed9a73455541f
az0325 <jy960325@gmail.com>
2019-11-13 08:56:23 +0900

    no message

--- navigation/stacks/company/CompanyBoardStack.js
+++ navigation/stacks/company/CompanyBoardStack.js
@@ -4,7 +4,6 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Header from "../../../components/Header";
 import transitionConfig from '../transitionConfig';
 
-
 import { CompanyBoardList, CompanyBoardDetail, CompanyBoardWrite } from '../../../screens/company';
 
 const CompanyBoardStack = createStackNavigator(
--- navigation/stacks/company/CompanyStack.js
+++ navigation/stacks/company/CompanyStack.js
@@ -5,15 +5,6 @@ import Header from "../../../components/Header";
 import transitionConfig from '../transitionConfig';
 
 import CompanyScreen from '../../../screens/company/CompanyScreen';
-import CompanyGalleryDetail from '../../../screens/company/gallery/CompanyGalleryDetail';
-import CompanyAddGallery from '../../../screens/company/gallery/CompanyAddGallery';
-import CompanyGalleryWrite from '../../../screens/company/gallery/CompanyGalleryWrite';
-
-import {
-    CompanyBoardList, CompanyBoardDetail,
-    CompanyGallery, CompanyBoardWrite,
-    CompanyCalendar,
-} from '../../../screens/company';
 
 const CompanyStack = createStackNavigator(
     {
@@ -23,62 +14,12 @@ const CompanyStack = createStackNavigator(
                 header: <Header search options title="회사" navigation={navigation} />
             })
         },
-        CompanyBoardList: {
-            screen: CompanyBoardList,
-            navigationOptions: ({ navigation }) => ({
-                header: <Header back={true} title="회사" navigation={navigation} />
-            })
-        },
-        CompanyBoardDetail: {
-            screen: CompanyBoardDetail,
-            navigationOptions: ({ navigation }) => ({
-                header: <Header back={true} title="회사" navigation={navigation} />
-            })
-        },
-        CompanyGallery: {
-            screen: CompanyGallery,
-            navigationOptions: ({ navigation }) => ({
-                header: <Header back={true} title="회사" navigation={navigation} />
-            })
-        },
-        CompanyGalleryDetail: {
-            screen: CompanyGalleryDetail,
-            navigationOptions: ({ navigation }) => ({
-                header: <Header back={true} title="회사" navigation={navigation} />
-            })
-        },
-        CompanyBoardWrite: {
-            screen: CompanyBoardWrite,
-            navigationOptions: {
-                header: null,
-                gesturesEnabled: false,
-            },
-        },
-        CompanyCalendar: {
-            screen: CompanyCalendar,
-            navigationOptions: ({ navigation }) => ({
-                header: <Header back={true} title="회사" navigation={navigation} />
-            })
-        },
-        CompanyAddGallery: {
-            screen: CompanyAddGallery,
-            navigationOptions: {
-                header: null,
-            }
-            
-        },
-        CompanyGalleryWrite: {
-            screen: CompanyGalleryWrite,
-            navigationOptions: {
-                header: null,
-            }
-        },
-        CompanyCamera: {
-            screen: CompanyCamera,
-            navigationOptions: {
-                header: null,
-            }
-        }
+        // CompanyCamera: {
+        //     screen: CompanyCamera,
+        //     navigationOptions: {
+        //         header: null,
+        //     }
+        // }
     },  
     {
         cardStyle: { backgroundColor: "#F8F9FE" },
