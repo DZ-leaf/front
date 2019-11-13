7a8b3ea073a4d9d7ecb70c6c92a861609d0f2096
jjieun07 <eejieun98@naver.com>
2019-11-13 19:12:23 +0900

    카메라

--- screens/company/gallery/CompanyAddGallery.js
+++ screens/company/gallery/CompanyAddGallery.js
@@ -13,8 +13,8 @@ class CompanyAddGallery extends Component {
         return (
             <Block flex>
                 {/* <Text>Gallery 추가 (카메라, 갤러리 접근)</Text> */}
-                {/* <Camera navigation={this.props.navigation}/> */}
-                <CameraRoll navigation={this.props.navigation}/>
+                <Camera navigation={this.props.navigation}/>
+                {/* <CameraRoll navigation={this.props.navigation}/> */}
             </Block>
         );
     }
