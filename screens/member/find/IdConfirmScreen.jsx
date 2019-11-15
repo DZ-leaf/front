fae0f3213b437e0448923333f99d4d10dfe6b667
az0325 <jy960325@gmail.com>
2019-11-15 10:03:51 +0900

    아이디 비번 찾기

--- screens/member/find/IdConfirmScreen.jsx
+++ screens/member/find/IdConfirmScreen.jsx
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Dimensions } from 'react-native';
 import { Block, Button, Text, theme, } from 'galio-framework';
 import { withNavigation } from 'react-navigation';
 
 const { width } = Dimensions.get("screen");
 
-class IdConfirmScreen extends Component {
-    render() {
-        const { navigation } = this.props;
+const IdConfirmScreen = (props) => {
 
-        const navigate = () => {
-            this.props.onClickListener()
-            navigation.navigate('Login')
-        }
+    const navigate = () => {
+        props.onClickListener()
+        props.navigation.navigate('Login')
+    }
 
-        return (
-            <Block flex>
-                <Block style={styles.container}>
-                    <Text style={styles.text}>이메일 정보와 일치하는 아이디입니다.</Text>
-                    <Block style={styles.msgContiner}>
-                        <Text style={{ fontSize: 15 }}>{navigation.getParam('memberId')}</Text>
-                    </Block>
-                    <Block middle>
-                        <Button style={styles.button} 
-                        onPress={() => { navigate() }} shadowless>확인</Button>
-                    </Block>
+    return (
+        <Block flex>
+            <Block style={styles.container}>
+                <Text style={styles.text}>이메일 정보와 일치하는 아이디입니다.</Text>
+                <Block style={styles.msgContiner}>
+                    <Text style={{ fontSize: 15 }}>{props.navigation.getParam('memberId')}</Text>
+                </Block>
+                <Block middle>
+                    <Button style={styles.button}
+                        onPress={navigate} shadowless>확인</Button>
                 </Block>
             </Block>
-        );
-    }
+        </Block>
+    );
 }
 
 const styles = StyleSheet.create({
